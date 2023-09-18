aa890deebb58e706880c33e7b1246759452aca23
geslic <b.geslicki@gmail.com>
2023-09-17 17:14:36 -0700

    first commit being made

---
+++ src/testProject/Application.java
@@ -0,0 +1,14 @@
+package testProject;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double value1 = 14.99;
+		double value2 = 15.99;
+		System.out.println(value1 + value2);
+		
+
+	}
+
+}
